54d05763e5680c7e1668d6a021192483b7024081
orlandotellez <espadachinkinno@gmail.com>
2026-03-30 18:15:30 -0600

    feat: agregar pagina de chats del usuario y su diseño y datos de prueba

--- app/(tabs)/chats/index.tsx
+++ app/(tabs)/chats/index.tsx
@@ -1,9 +1,181 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { THEME } from "@/src/shared/lib/theme";
+import { ChatItem } from "@/src/features/chats/components/ChatItem";
+import type { IChatItem } from "@/src/shared/types/chats"
+import { chats } from "@/src/shared/data/chats";
 
 export default function ChatScreen() {
   return (
-    <View>
-      <Text>Chats</Text>
+    <View style={styles.container}>
+      {/* HEADER */}
+      <View style={styles.header}>
+        <Text style={styles.title}>Anteny App</Text>
+        <View style={styles.headerIcons}>
+          <Ionicons name="search" size={22} color={THEME.colors.text_opacity} />
+          <MaterialIcons name="more-vert" size={22} color={THEME.colors.text_opacity} />
+        </View>
+      </View>
+
+      {/* LIST */}
+      <FlatList<IChatItem>
+        data={chats}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <ChatItem
+            id={item.id}
+            avatar={item.avatar}
+            name={item.name}
+            message={item.message}
+            time={item.time}
+            unread={item.unread}
+            online={item.online}
+          />
+        )}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      />
+
+      {/* FAB */}
+      <TouchableOpacity style={styles.fab}>
+        <Ionicons name="chatbubble" size={24} color="#002109" />
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: THEME.colors.background,
+  },
+
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 16,
+  },
+
+  title: {
+    color: THEME.colors.primary,
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+
+  headerIcons: {
+    flexDirection: "row",
+    gap: 16,
+  },
+  chatItem: {
+    flexDirection: "row",
+    padding: 16,
+    alignItems: "center",
+  },
+
+  avatar: {
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+  },
+
+  onlineDot: {
+    position: "absolute",
+    bottom: 2,
+    right: 2,
+    width: 10,
+    height: 10,
+    backgroundColor: THEME.colors.primary,
+    borderRadius: 5,
+  },
+
+  chatContent: {
+    flex: 1,
+    marginLeft: 12,
+  },
+
+  chatHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  name: {
+    color: THEME.colors.text_title,
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+
+  time: {
+    color: THEME.colors.text_opacity,
+    fontSize: 12,
+  },
+
+  messageRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 4,
+  },
+
+  message: {
+    color: THEME.colors.text_opacity,
+    flex: 1,
+  },
+
+  badge: {
+    backgroundColor: THEME.colors.primary,
+    borderRadius: 10,
+    paddingHorizontal: 6,
+    justifyContent: "center",
+  },
+
+  badgeText: {
+    color: "#002109",
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+
+  fab: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    backgroundColor: THEME.colors.primary,
+    width: 56,
+    height: 56,
+    borderRadius: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  bottomNav: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingVertical: 10,
+    backgroundColor: "#131313",
+  },
+
+  bottomItem: {
+    alignItems: "center",
+  },
+
+  bottomItemActive: {
+    alignItems: "center",
+  },
+
+  bottomText: {
+    color: THEME.colors.text_opacity,
+    fontSize: 10,
+  },
+
+  bottomTextActive: {
+    color: THEME.colors.primary,
+    fontSize: 10,
+  },
+});
+
---
+++ src/shared/data/chats.ts
@@ -0,0 +1,21 @@
+export const chats = [
+  {
+    id: "1",
+    name: "Alex Rivera",
+    message: "Sent the finalized design system assets...",
+    time: "10:45 AM",
+    unread: 3,
+    online: true,
+    avatar:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuBfL7KRipuqhUiNMOlmwchX8fkOOjutq9HwQnG6SZL-bHGvleem88atMv0HnBBVwKb_fkY-eosBSK08XXsQJd5J68nNcUz7DbE7-ZveGR23MNwQrs5dJgW53llc6pm8jpSfE0tQj9SK3M7VlDFEAp1O4ClYw_B3avSbidzwrjzDFYhs06F-N3AqmOIEgnWV1TX2STvevkrsNwrlQ9K5ZX1iCK0Fdd1pH41acxgm333i8EIHDwNHZ16-3e5T7Zk1i3DTgj-TRToicNg",
+  },
+  {
+    id: "2",
+    name: "Design Squad 🎨",
+    message: "Sarah: The Midnight Monolith looks incredible!",
+    time: "Yesterday",
+    avatar:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuAebinuJKUjTpXCAUw3F2am3C7J1t8M-V5vq0nSLNxHdjQPDp6DfuqUbwuuOkzMZCZcgqr9vpQt6ilbVLH2XJOLxX0Q8CXs_xl60NmNDblgwJu0oKoecLmveVrf3mAsp1IOjlDTnnW-6rTF6iCcJQugJcWCDTU-MJaxIE3AD8R3-hTXaSDuyp3Ip0cO36nyrzmdDAMoPVf8ZZXbpq3oEJz1RgtuP9_pDdYUuQUY3TlOB5evn5tvHhEj9tT6GbrTHlUYImf0-qG11Xs",
+  }
+];
+
