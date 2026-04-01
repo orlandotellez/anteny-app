cf8663a6e6eeaf220fb9da48134909c1c25a5c42
orlandotellez <espadachinkinno@gmail.com>
2026-03-31 18:43:34 -0600

    refactor: separar el header de contacts

--- app/(tabs)/contacts/index.tsx
+++ app/(tabs)/contacts/index.tsx
@@ -1,35 +1,23 @@
 import React from "react";
 import {
-  View,
-  Text,
   StyleSheet,
   FlatList,
 } from "react-native";
-import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { THEME } from "@/src/shared/lib/theme";
 import { contacts } from "@/src/shared/data/contacts";
 import type { IContactItem } from "@/src/shared/types/contacts";
 import { ContactItem } from "@/src/features/contacts/components/ContactItem";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ActionsButtons } from "@/src/features/contacts/components/ActionsButtons";
+import { Header } from "@/src/features/contacts/components/Header";
 
 export default function ContactScreen() {
   return (
     <SafeAreaView style={styles.container} edges={["top"]}>
       {/* HEADER */}
-      <View style={styles.header}>
-        <View style={styles.headerLeft}>
-          <View>
-            <Text style={styles.title}>Contacts</Text>
-          </View>
-        </View>
+      <Header />
 
-        <View style={styles.headerRight}>
-          <Ionicons name="search" size={22} color={THEME.colors.text_opacity} />
-          <MaterialIcons name="more-vert" size={22} color={THEME.colors.text_opacity} />
-        </View>
-      </View>
-
+      {/* ACTIONS BUTTONS */}
       <ActionsButtons />
 
       {/* LIST */}
@@ -47,29 +35,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: THEME.colors.background,
-  },
-
-  header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    backgroundColor: THEME.colors.secondary,
-    padding: 16,
-  },
-
-  headerLeft: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 12,
-  },
-
-  headerRight: {
-    flexDirection: "row",
-    gap: 16,
-  },
-  title: {
-    color: THEME.colors.primary,
-    fontSize: 22,
-    fontWeight: "bold",
   },
   subtitle: {
     color: THEME.colors.text_opacity,
---
+++ src/features/contacts/components/Header.tsx
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, View } from "react-native"
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { THEME } from "@/src/shared/lib/theme";
+
+export const Header = () => {
+  return (
+    <>
+      <View style={styles.header}>
+        <View style={styles.headerLeft}>
+          <View>
+            <Text style={styles.title}>Contacts</Text>
+          </View>
+        </View>
+
+        <View style={styles.headerRight}>
+          <Ionicons name="search" size={22} color={THEME.colors.text_opacity} />
+          <MaterialIcons name="more-vert" size={22} color={THEME.colors.text_opacity} />
+        </View>
+      </View>
+
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: THEME.colors.secondary,
+    padding: 16,
+  },
+  headerLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+  headerRight: {
+    flexDirection: "row",
+    gap: 16,
+  },
+  title: {
+    color: THEME.colors.primary,
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+})
